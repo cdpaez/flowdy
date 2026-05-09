4d04247e5b9ef543b3f144fb16ddc51c4e471086
cdpaez <archivos1789@gmail.com>
2026-05-09 16:42:18 -0500

    fix: express proxy

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const app = express();
 
 // Middlewares
 app.use("/api", apiLimiter);
+app.set('trust proxy', 1);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
